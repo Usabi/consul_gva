fcd5122ae38c2d2ed2f7a55a5dbb61bec511a6ce
Alejandro <alejandro.vera1988@gmail.com>
2021-11-02 07:56:02 +0000

    Upgrato to version 1.3.1

---
+++ app/components/custom/admin/budgets/index_component.rb
@@ -0,0 +1,6 @@
+class Admin::Budgets::IndexComponent < ApplicationComponent; end
+
+require_dependency Rails.root.join("app", "components", "admin","budgets", "index_component")
+
+class Admin::Budgets::IndexComponent
+end
