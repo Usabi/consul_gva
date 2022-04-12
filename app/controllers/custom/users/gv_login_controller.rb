121c17e8d182fcbf5a86d2f011ada53adb0d2f24
Alejandro <alejandro.vera1988@gmail.com>
2022-04-12 14:06:01 +0100

    Add user guide for login

--- app/controllers/custom/users/gv_login_controller.rb
+++ app/controllers/custom/users/gv_login_controller.rb
@@ -40,35 +40,43 @@ class Users::GvLoginController < ApplicationController
         # Add guide connection codper
         auth = OpenStruct.new(uid: uid, provider: :gvlogin)
         identity = Identity.first_or_create_from_oauth(auth)
-        @user = identity.user.present? && identity.user.valid? ? identity.user : User.first_or_initialize_for_gvlogin(data)
+        @user = get_or_create_user(data, identity)
         identity.update!(user: @user)
-
-        if @user.save
-          role = data.roles.present? ? data.roles&.dig(:role, :codigo) : ""
-          if role == "PARTICIPEM_ADMIN"
-            @user.send("create_#{GVA_ROLES[role]}") unless @user.send("#{GVA_ROLES[role]}?")
-            # Only sign in if admin right now
-            # Move sign_in outside of if when other roles added
-            flash[:success] = I18n.t("devise.sessions.signed_in")
-            sign_in_and_redirect @user, event: :authentication
-          else
-            @user.administrator.delete if @user.administrator?
-            flash[:error] = I18n.t("devise.failure.no_backend_roles")
-            delete_cookies
-            redirect_to new_user_session_path
-          end
-        else
-          errors = @user.errors
-          flash[:error] = errors.messages.map {|field, error| "#{field}: #{error.join(", ")} ('#{errors.details[field][0][:value]}')" }.join(", ")
-          delete_cookies
-
-          redirect_to new_user_session_path
-        end
+        save_user(data)
+        return
       else
-        flash[:error] = I18n.t("devise.failure.no_codeper_email")
-        delete_cookies
-        redirect_to new_user_session_path
+        if isEmailGVA?(data.mail)
+          vmcrc_user = VmcrcPersona.find_by(dni: data.dni)
+          if !vmcrc_user.present? || vmcrc_user.codper.blank?
+            error_user_data
+            return
+          end
+          auth = OpenStruct.new(uid: vmcrc_user.codper, provider: :gvlogin)
+          identity = Identity.first_or_create_from_oauth(auth)
+        elsif (uid = data.info_ampliada["codper"].presence)
+          vmcrc_user = VmcrcPersona.find_by(codper: uid)
+          # byebug
+          if !vmcrc_user.present? || vmcrc_user.dcorreo.blank?
+            error_user_data
+            return
+          end
+          auth = OpenStruct.new(uid: uid, provider: :gvlogin)
+          identity = Identity.first_or_create_from_oauth(auth)
+          data.mail = vmcrc_user.dcorreo
+        else
+          vmcrc_user = VmcrcPersona.find_by(dni: data.dni)
+          if !vmcrc_user.present? || vmcrc_user.dcorreo.blank? || vmcrc_user.codper.blank?
+            error_user_data
+            return
+          end
+          auth = OpenStruct.new(uid: vmcrc_user.codper, provider: :gvlogin)
+          identity = Identity.first_or_create_from_oauth(auth)
+          data.mail = vmcrc_user.dcorreo
+        end
       end
+      @user = get_or_create_user(data, identity)
+      identity.update!(user: @user)
+      save_user(data)
     end
 
     def isEmailGVA?(email)
@@ -77,5 +85,54 @@ class Users::GvLoginController < ApplicationController
 
     def delete_cookies
       cookies.delete("gvlogin.login.GVLOGIN_COOKIE", domain: request.domain)
+    end
+
+    def get_or_create_user(data, identity)
+      identity.user.present? && identity.user.valid? ? identity.user : User.first_or_initialize_for_gvlogin(data)
+    end
+
+    def success_admin_login
+      flash[:success] = I18n.t("devise.sessions.signed_in")
+
+      sign_in_and_redirect @user, event: :authentication
+    end
+
+    def error_admin_login
+      @user.administrator.delete if @user.administrator?
+      flash[:error] = I18n.t("devise.failure.no_backend_roles")
+      delete_cookies
+
+      redirect_to new_user_session_path
+    end
+
+    def error_save_user
+      errors = @user.errors
+      flash[:error] = errors.messages.map { |field, error| "#{field}: #{error.join(", ")} ('#{errors.details[field][0][:value]}')" }.join(", ")
+      delete_cookies
+
+      redirect_to new_user_session_path
+    end
+
+    def error_user_data
+      flash[:error] = I18n.t("devise.failure.no_codeper_email")
+      delete_cookies
+
+      redirect_to new_user_session_path
+    end
+
+    def save_user(data)
+      if @user.save
+        role = data.roles.present? ? data.roles&.dig(:role, :codigo) : ""
+        if role == "PARTICIPEM_ADMIN"
+          @user.send("create_#{GVA_ROLES[role]}") unless @user.send("#{GVA_ROLES[role]}?")
+          # Only sign in if admin right now
+          # Move sign_in outside of if when other roles added
+          success_admin_login
+        else
+          error_admin_login
+        end
+      else
+        error_save_user
+      end
     end
  end
