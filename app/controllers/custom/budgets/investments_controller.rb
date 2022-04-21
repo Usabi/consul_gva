6403ffd3aa4e58077ae43e3c0e9e6cacfd2ffa14
Alejandro <alejandro.vera1988@gmail.com>
2022-04-21 17:37:50 +0100

    Update custom files to 1.4.1

--- app/controllers/custom/budgets/investments_controller.rb
+++ app/controllers/custom/budgets/investments_controller.rb
@@ -2,12 +2,27 @@ require_dependency Rails.root.join('app', 'controllers', 'budgets', 'investments
 class Budgets::InvestmentsController
   # Rails.application.load_tasks # NOTE: En Valencia lo tienen pero no sé si es necesario (igual hace falta si pasa algo con tareas)
 
+  PER_PAGE = 20
+
   before_action :load_headings, only: [:index, :new, :create, :vote, :unvote]
   before_action :load_votes, only: [:index, :show]
   before_action :load_categories, only: [:index, :new, :create, :vote, :unvote, :edit, :update]
 
   valid_filters = %w[not_unfeasible feasible unfeasible unselected selected winners not_selected]
   has_filters valid_filters, only: [:index, :show, :suggest]
+
+  def index
+    session["create_investment_back_path"] = request.fullpath
+    session["ballot_referer"] = request.env["REQUEST_URI"]
+    @back_link_map_path = session[:back_link_map_path].present? ? session[:back_link_map_path] : budget_path(@budget)
+    session[:back_link_investment_list] = request.fullpath
+    @investments = investments.page(params[:page]).per(PER_PAGE).for_render
+    @investment_ids = @investments.ids
+    @investments_map_coordinates = MapLocation.where(investment: investments).map(&:json_data)
+    @tag_cloud = tag_cloud
+    @remote_translations = detect_remote_translations(@investments)
+  end
+
 
   def unvote
     @investment.register_selection_vote_and_unvote(current_user, "no")
@@ -31,22 +46,11 @@ class Budgets::InvestmentsController
     end
   end
 
-  def index
-    investments_per_page = 20
-    session["create_investment_back_path"] = request.fullpath
-    session["ballot_referer"] = request.env["REQUEST_URI"]
-    @back_link_map_path = session[:back_link_map_path].present? ? session[:back_link_map_path] : budget_path(@budget)
-    session[:back_link_investment_list] = request.fullpath
-    @investments = investments.page(params[:page]).per(investments_per_page).for_render
 
-    @investment_ids = @investments.pluck(:id)
-    load_investment_votes(@investments)
-    @tag_cloud = tag_cloud
-  end
 
   private
     def load_categories
-      @categories = ActsAsTaggableOn::Tag.category.order(name: :desc)
+      @categories = Tag.category.order(name: :desc)
     end
 
     def load_headings
