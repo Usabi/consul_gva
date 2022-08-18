40a4cd9fde6a9bcb744c13c66bd600a5da8561c7
Alejandro <alejandro.vera1988@gmail.com>
2022-08-18 11:58:00 +0100

    Fix tabs navigation and add advance search

--- app/models/custom/legislation/process.rb
+++ app/models/custom/legislation/process.rb
@@ -5,6 +5,10 @@ class Legislation::Process
 
   belongs_to :user, optional: true, inverse_of: :legislation_processes
 
+  def self.processes_filters
+    %w[preview_phase public_phase past relevance]
+  end
+
   scope :preview_phase, -> {
     where("(debate_phase_enabled = true and (debate_start_date <= :date and debate_end_date >= :date)) or
           (proposals_phase_enabled = true and (proposals_phase_start_date <= :date and proposals_phase_end_date >= :date))", date: Date.current)
