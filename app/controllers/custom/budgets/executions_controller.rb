7e47fc24f70a97360e38e75044e538b99d0d4974
Ignacio Aliende García <ialiendeg@gmail.com>
2023-06-04 09:43:28 +0100

    feat: Add projects filter in budget executions
    See #5841

---
+++ app/controllers/custom/budgets/executions_controller.rb
@@ -0,0 +1,19 @@
+require_dependency Rails.root.join('app', 'controllers', 'budgets', 'executions_controller').to_s
+
+class Budgets::ExecutionsController
+  private
+
+    def investments_by_heading
+      base = @budget.investments.winners.apply_filters_and_search(@budget, params, @current_filter)
+      base = base.joins(milestones: :translations).includes(:milestones)
+      base = base.tagged_with(params[:milestone_tag]) if params[:milestone_tag].present?
+
+      if params[:status].present?
+        base = base.with_milestone_status_id(params[:status])
+        base.uniq.group_by(&:heading)
+      else
+        base.uniq.group_by(&:heading)
+      end
+    end
+end
+
