6b411891e3dbfda6860f8c879dc94d3e4a921d8e
Adán Alonso Salvador <adan.alonso.s@gmail.com>
2023-06-07 10:22:54 +0100

    Check if column exists before removing it
    Signed-off-by: Adán Alonso Salvador <adan.alonso.s@gmail.com>

--- db/migrate/20220224173405_remove_original_filename_from_documents.rb
+++ db/migrate/20220224173405_remove_original_filename_from_documents.rb
@@ -1,5 +1,5 @@
 class RemoveOriginalFilenameFromDocuments < ActiveRecord::Migration[5.1]
   def change
-    remove_column :documents, :original_filename
+    remove_column :documents, :original_filename if column_exists?(:documents, :original_filename)
   end
 end
