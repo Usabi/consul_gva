514122d010a039b44dcf03b466e1073c8a1b0c2b
Ignacio Aliende García <ialiendeg@gmail.com>
2023-06-11 10:34:44 +0100

    feat: Change budget header design
    Add normal section header and reduce current budget
    header height
    
    See #5841

---
+++ app/components/custom/budgets/budget_component.rb
@@ -0,0 +1,6 @@
+class Budgets::BudgetComponent < ApplicationComponent; end
+
+require_dependency Rails.root.join("app", "components", "budgets", "budget_component").to_s
+
+class Budgets::BudgetComponent < ApplicationComponent
+end
