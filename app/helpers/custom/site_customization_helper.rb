65c4a6d529eb5b3d6619a43beb2d6fa6141536c4
Alejandro <alejandro.vera1988@gmail.com>
2024-05-23 12:08:02 +0100

    feat: Modifico el componente custom para que incluya los arial

--- app/helpers/custom/site_customization_helper.rb
+++ app/helpers/custom/site_customization_helper.rb
@@ -1,5 +1,5 @@
 module Custom::SiteCustomizationHelper
   def custom_information_texts_tabs
-    [:basic, :debates, :community, :proposals, :polls, :layouts, :mailers, :management, :welcome, :application, :dashboard]
+    [:basic, :debates, :community, :proposals, :polls, :legislation, :budgets, :layouts, :mailers, :management, :welcome, :application, :dashboard, :machine_learning]
   end
 end
