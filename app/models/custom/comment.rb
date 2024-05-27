066cb76e75c1c4683ce10e6b58784a5b24f71efb
Alejandro <alejandro.vera1988@gmail.com>
2024-05-27 12:06:05 +0100

    fix: Corrijo query para que busque comentarios que no son de usuarios admin

--- app/models/custom/comment.rb
+++ app/models/custom/comment.rb
@@ -2,7 +2,7 @@ require_dependency Rails.root.join("app", "models", "comment").to_s
 
 class Comment
   scope :not_as_admin_or_moderator, -> do
-    where("administrator_id IS NULL OR legislator_id IS NULL OR budget_manager_id IS NULL").where(moderator_id: nil)
+    where("administrator_id IS NULL AND legislator_id IS NULL AND budget_manager_id IS NULL").where(moderator_id: nil)
   end
 
   def as_administrator?
