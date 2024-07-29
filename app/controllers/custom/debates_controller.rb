f29157309335cbce5101363c0306631defe218c2
Alejandro <alejandro.vera1988@gmail.com>
2024-07-29 12:07:03 +0100

    feat: Creo vista de map como en propuestas y añadi el mapa en el sidebar del listado de debates

--- app/controllers/custom/debates_controller.rb
+++ app/controllers/custom/debates_controller.rb
@@ -1,6 +1,13 @@
 require_dependency Rails.root.join("app", "controllers", "debates_controller").to_s
 
 class DebatesController
+  before_action :load_geozones, only: [:edit, :map]
+
+  def map
+    @debate = Debate.new
+    @tag_cloud = tag_cloud
+  end
+
   private
 
     def allowed_params
