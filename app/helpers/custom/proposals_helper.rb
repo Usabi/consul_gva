5692c64dbd9edd6d66e4f099c609d34383afdc14
Alberto Clavijo <alberto.clavijo@metodo.tech>
2025-02-10 18:02:38 +0000

    feat: Add help_text tab within admin proposals

--- app/helpers/custom/proposals_helper.rb
+++ app/helpers/custom/proposals_helper.rb
@@ -2,4 +2,14 @@ module Custom::ProposalsHelper
   def budget_phase_selecting?
     Budget.find_by(phase: "selecting").present?
   end
+
+  def proposals_tabs()
+    tabs = {
+      "proposals" => admin_proposals_path,
+      }
+    if current_user&.administrator? || (current_user&.legislator? && current_user == process.user)
+      tabs = tabs.merge({ 'help_text' => help_page_admin_proposals_path })
+    end
+    tabs
+  end
 end
