64e84f5b4c4fb5b8f4bd36e9e7433a0def36f212
Alberto Clavijo <alberto.clavijo@metodo.tech>
2025-02-11 18:34:56 +0000

    feat: Not allow to edit title and section, and add full options to ckeditor in content

--- app/assets/javascripts/custom/html_editor.js
+++ app/assets/javascripts/custom/html_editor.js
@@ -2,13 +2,10 @@
   "use strict";
 
   App.HTMLEditor.initialize = function() {
-    debugger
     $("textarea.html-area").each(function() {
       if ($(this).hasClass("admin")) {
-        debugger
         CKEDITOR.replace(this.name, { language: $("html").attr("lang"), toolbar: "admin", height: 500 });
       } else if($(this).hasClass("custom")) {
-        debugger
         CKEDITOR.replace(this.name, { language: $("html").attr("lang"), toolbar: "page", height: 500 });
       } else  {
         CKEDITOR.replace(this.name, { language: $("html").attr("lang") });
