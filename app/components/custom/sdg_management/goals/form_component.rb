c1ac08a5de20503516c87d1e7278f186ee00b2e1
Alejandro Vera <alejandro.vera1988@gmail.com>
2025-03-07 14:51:45 +0000

    chore: Update from the original code version

--- app/components/custom/sdg_management/goals/form_component.rb
+++ app/components/custom/sdg_management/goals/form_component.rb
@@ -9,4 +9,14 @@ class SDGManagement::Goals::FormComponent < ApplicationComponent
     @goal = goal
     @contents = [goal.content]
   end
+
+  private
+
+    def translation_enabled_tag(locale, enabled)
+      hidden_field_tag("enabled_translations[#{locale}]", (enabled ? 1 : 0))
+    end
+
+    def enabled_locales
+      Setting.enabled_locales
+    end
 end
