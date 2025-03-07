6aff5f22d0353d2f3b11dcfb8a6e3875e57bac5d
Alberto Clavijo <alberto.clavijo@metodo.tech>
2025-03-07 02:12:37 +0000

    feat: Add new admin page key_systems

--- app/models/custom/abilities/administrator.rb
+++ app/models/custom/abilities/administrator.rb
@@ -81,6 +81,8 @@ module Abilities
 
       can :read_admin_stats, Budget, &:balloting_or_later?
 
+      can :index, KeySystem
+
       can [:search, :update, :create, :index, :destroy], Banner
 
       can [:search, :update, :create, :index, :destroy], AlertMessage
