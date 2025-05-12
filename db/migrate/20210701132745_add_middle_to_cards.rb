dd00cf43db71b781f0ac401ef5d1c1872d95cb89
Alejandro Vera <alejandro.vera1988@gmail.com>
2025-05-12 10:56:46 +0100

    feat: Cambiar platform

--- db/migrate/20210701132745_add_middle_to_cards.rb
+++ db/migrate/20210701132745_add_middle_to_cards.rb
@@ -16,39 +16,39 @@ class AddMiddleToCards < ActiveRecord::Migration[5.1]
     add_column :widget_cards, :middle, :boolean, default: false
 
     Widget::Card.reset_column_information
-    Widget::Card.create!(
-      middle: true,
-      label_val: 'middle_left_val',
-      title_val: 'Secció central esquerra',
-      link_text_val: 'Informació i debats',
-      link_url_val: 'http://participem.gva.es/va/fase-1-informacio',
-      link_text_2_val: 'Dona Suport a la teua proposta! ',
-      link_url_2_val: 'https://gvaparticipa.gva.es/budgets',
-      image_attributes: create_image_attachment(:val, :text)
-    )
-    Widget::Card.create!(
-      middle: true,
-      label_val: 'middle_right_val',
-      title_val: 'Secció central dreta',
-      image_attributes: create_image_attachment(:val, :image),
-    )
+    # Widget::Card.create!(
+    #   middle: true,
+    #   label_val: 'middle_left_val',
+    #   title_val: 'Secció central esquerra',
+    #   link_text_val: 'Informació i debats',
+    #   link_url_val: 'http://participem.gva.es/va/fase-1-informacio',
+    #   link_text_2_val: 'Dona Suport a la teua proposta! ',
+    #   link_url_2_val: 'https://gvaparticipa.gva.es/budgets',
+    #   image_attributes: create_image_attachment(:val, :text)
+    # )
+    # Widget::Card.create!(
+    #   middle: true,
+    #   label_val: 'middle_right_val',
+    #   title_val: 'Secció central dreta',
+    #   image_attributes: create_image_attachment(:val, :image),
+    # )
 
-    Widget::Card.create!(
-      middle: true,
-      label_es: 'middle_left_es',
-      title_es: 'Sección central izquierda',
-      link_text_es: 'Información y debates',
-      link_url_es: 'http://participem.gva.es/es/fase-1-informacio',
-      link_text_2_es: '¡Apoya tu propuesta!',
-      link_url_2_es: 'https://gvaparticipa.gva.es/budgets',
-      image_attributes: create_image_attachment(:es, :text)
-    )
-    Widget::Card.create!(
-      middle: true,
-      label_es: 'middle_right_es',
-      title_es: 'Sección central derecha',
-      image_attributes: create_image_attachment(:es, :image),
-    )
+    # Widget::Card.create!(
+    #   middle: true,
+    #   label_es: 'middle_left_es',
+    #   title_es: 'Sección central izquierda',
+    #   link_text_es: 'Información y debates',
+    #   link_url_es: 'http://participem.gva.es/es/fase-1-informacio',
+    #   link_text_2_es: '¡Apoya tu propuesta!',
+    #   link_url_2_es: 'https://gvaparticipa.gva.es/budgets',
+    #   image_attributes: create_image_attachment(:es, :text)
+    # )
+    # Widget::Card.create!(
+    #   middle: true,
+    #   label_es: 'middle_right_es',
+    #   title_es: 'Sección central derecha',
+    #   image_attributes: create_image_attachment(:es, :image),
+    # )
   end
 
   def down
