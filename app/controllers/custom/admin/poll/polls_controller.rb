f537c1d84e29953ff2571f910fa667dcc1061e67
Alejandro Vera <alejandro.vera1988@gmail.com>
2025-06-04 11:16:48 +0100

    feat: Add filter to admin polls

--- app/controllers/custom/admin/poll/polls_controller.rb
+++ app/controllers/custom/admin/poll/polls_controller.rb
@@ -1,10 +1,22 @@
 require_dependency Rails.root.join("app", "controllers", "admin", "poll", "polls_controller").to_s
 
 class Admin::Poll::PollsController
+  include CommentableActions
   include Admin::HelpPagesActions
 
   def help_page
     @help_text_content = help_text(controller_name)
     render "admin/poll/polls/help_pages/show"
+  end
+
+  has_filters %w[all current expired], only: :index
+
+  def index
+    @polls = ::Poll.not_budget.created_by_admin.scoped_filter(params, @current_filter, @advanced_search_terms).order_filter(params)
+    @polls = Kaminari.paginate_array(@polls) if @polls.is_a?(Array)
+    @polls = @polls.page(params[:page])
+    respond_to do |format|
+      format.html
+    end
   end
 end
