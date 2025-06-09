5e51d851a4c862dc6528f8f0b547c6949196befb
Alejandro Vera <alejandro.vera1988@gmail.com>
2025-06-09 07:52:16 +0100

    feat: Añadir  extensión trgm

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2025_02_25_164500) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
   enable_extension "unaccent"
 
