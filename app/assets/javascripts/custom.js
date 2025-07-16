25089e632ad67604d60126d8f7cd1d0499c641f4
Alejandro Vera <alejandro.vera1988@gmail.com>
2025-07-16 13:04:35 +0100

    feat: Disable events and add new style

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,49 +5,47 @@
 //
 // See the `docs/en/customization/javascript.md` file for more information.
 
-var initialize_modules = function() {
-  "use strict";
+// var initialize_modules = function() {
+//   "use strict";
 
-  // Add calls to your custom code here; this will be called when
-  // loading a page.
+//   // Add calls to your custom code here; this will be called when
+//   // loading a page.
 
-  //= require custom/welcome_counter
-  //= require custom/select_local
+//   //= require custom/welcome_counter
+//   //= require custom/select_local
 
-  (function() {
-    "use strict";
+//   // (function() {
 
-    App = App || {};
+//   //   App = App || {};
 
-    App.Investments = {
-      initializeSelection: function() {
-        var selectAllButton = document.getElementById('select-all');
-        var checkboxes = document.querySelectorAll('input[type="checkbox"][name="investment_ids[]"]');
+//   //   App.Investments = {
+//   //     initializeSelection: function() {
+//   //       var selectAllButton = document.getElementById('select-all');
+//   //       var checkboxes = document.querySelectorAll('input[type="checkbox"][name="investment_ids[]"]');
 
-        selectAllButton.addEventListener('click', function() {
-          checkboxes.forEach(function(checkbox) {
-            checkbox.checked = true;
-          });
-        });
-      },
-      initialize: function() {
-        this.initializeSelection();
-      }
-    };
+//   //       selectAllButton.addEventListener('click', function() {
+//   //         checkboxes.forEach(function(checkbox) {
+//   //           checkbox.checked = true;
+//   //         });
+//   //       });
+//   //     },
+//   //     initialize: function() {
+//   //       this.initializeSelection();
+//   //     }
+//   //   };
 
-    document.addEventListener('DOMContentLoaded', function() {
-      App.Investments.initialize();
-    });
-  }).call(this);
+//   //   document.addEventListener('DOMContentLoaded', function() {
+//   //     App.Investments.initialize();
+//   //   });
+//   // }).call(this);
+// };
 
-};
+// var destroy_non_idempotent_modules = function() {
+//   "use strict";
 
-var destroy_non_idempotent_modules = function() {
-  "use strict";
+//   // Add calls to your custom code here when your JavaScript code added
+//   // in `initialize_modules` is not idempotent.
+// };
 
-  // Add calls to your custom code here when your JavaScript code added
-  // in `initialize_modules` is not idempotent.
-};
-
-$(document).on("turbolinks:load", initialize_modules);
-$(document).on("turbolinks:before-cache", destroy_non_idempotent_modules);
+// $(document).on("turbolinks:load", initialize_modules);
+// $(document).on("turbolinks:before-cache", destroy_non_idempotent_modules);
