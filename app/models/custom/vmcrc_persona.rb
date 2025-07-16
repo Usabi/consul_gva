ed474906f72563e5da0c3123fdb8003fb472260e
Alejandro Vera <alejandro.vera1988@gmail.com>
2025-07-16 14:16:59 +0100

    feat: Change schema for vmcrc persona

--- app/models/custom/vmcrc_persona.rb
+++ app/models/custom/vmcrc_persona.rb
@@ -1,3 +1,3 @@
 class VmcrcPersona < ApplicationRecord
-  self.table_name = "owcrc.vmcrc_siac_personas"
+  self.table_name = (Rails.env.test? || Rails.env.development?) ? "vmcrc_siac_personas" : "owcrc.vmcrc_siac_personas"
 end
