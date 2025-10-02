4c8e341bbfd1ffbecf49a87b659a1d8358e07313
Alejandro Vera <alejandro.vera1988@gmail.com>
2025-10-02 08:44:04 +0100

    feat: Add new tol on gvLogin and test

--- spec/support/common_actions/custom/helpers.rb
+++ spec/support/common_actions/custom/helpers.rb
@@ -6,4 +6,32 @@ module Helpers
       find("label[for=local-#{lang}]").click
     end
   end
+
+  def gvlogin_response_with_roles(role_values)
+    role_payload =
+      if role_values.is_a?(Array)
+        { role: role_values.map do |rv|
+          { codigo: "PARTICIPEM_CON",
+            parametros: { parametro: { valorParametro: rv, nombreParametro: "ROL" }}}
+        end }
+      else
+        { role: { codigo: "PARTICIPEM_CON",
+                  parametros: { parametro: { valorParametro: role_values, nombreParametro: "ROL" }}}}
+      end
+
+    GvLoginApi::Response.new({
+      resultado: true,
+      datos: {
+        mail: data.email,
+        roles: role_payload,
+        nombre: data.name,
+        infoAmpliada: {
+          parametro: [
+            { valorParametro: data.codper, nombreParametro: "codper" }
+          ]
+        },
+        dni: data.dni
+      }
+    }.to_json)
+  end
 end
