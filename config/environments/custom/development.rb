817c1c28a6b09126a11e7f63aa8859ba6950c7da
Alberto Clavijo <albert.cla@gmail.com>
2025-10-02 13:09:01 +0100

    fix: Add tom-select as gem and change js_comrpessor to terser

--- config/environments/custom/development.rb
+++ config/environments/custom/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
   # Overwrite settings for the development environment or add your own
   # custom settings for this environment.
+  config.assets.js_compressor = :terser
 end
