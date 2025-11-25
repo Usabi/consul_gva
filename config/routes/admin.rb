c3f24f39b207f21ed158e0da5a6e25479a3fd270
Alberto Clavijo <albert.cla@gmail.com>
2025-11-25 17:14:40 +0000

    feat: add changelog in admin namespace

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -348,6 +348,8 @@ namespace :admin do
       resources :administrator_tasks, only: [:index, :edit, :update]
     end
 
+    resource :changelog, only: [:show] # Custom
+
     resources :local_census_records
     namespace :local_census_records do
       resources :imports, only: [:new, :create, :show]
