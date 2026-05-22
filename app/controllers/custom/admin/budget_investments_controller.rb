bc54c525649e046259c325b48ea663898668cc7c
Alejandro Vera <avera@usabi.es>
2026-05-22 14:36:42 +0100

    security: sustituir send(params[:button]) por case/when explícito en bulk_actions

--- app/controllers/custom/admin/budget_investments_controller.rb
+++ app/controllers/custom/admin/budget_investments_controller.rb
@@ -3,14 +3,12 @@ load Rails.root.join("app", "controllers", "admin", "budget_investments_controll
 class Admin::BudgetInvestmentsController
   before_action :load_investment, only: [:show, :edit, :update, :toggle_selection, :toggle_winner]
 
-  BULK_ACTIONS = %w[
-    visible_to_valuators_bulk
-    selected_bulk
-    winner_bulk
-  ].freeze
-
   def bulk_actions
-    send(params[:button]) if params[:button].in?(BULK_ACTIONS)
+    case params[:button]
+    when "visible_to_valuators_bulk" then visible_to_valuators_bulk
+    when "selected_bulk"             then selected_bulk
+    when "winner_bulk"               then winner_bulk
+    end
   end
 
   def toggle_winner
