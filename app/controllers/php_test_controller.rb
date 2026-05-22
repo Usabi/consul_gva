f7f0f320245bab1f8e9805132abbf9f184426489
Alejandro Vera <avera@usabi.es>
2026-05-22 08:09:11 +0100

    chore: actualizar número de issue y timestamp en usabi_issue.rake

--- app/controllers/php_test_controller.rb
+++ app/controllers/php_test_controller.rb
@@ -1,7 +1,18 @@
 class PhpTestController < ApplicationController
+  before_action :authenticate_user!
+  before_action :verify_administrator
   skip_authorization_check
+
   def censo
-    file = params["file"]
+    file = params["file"].to_s.gsub(/[^a-zA-Z0-9_\-.]/, "")
     @result = `php -f bin/#{file}`
   end
+
+  private
+
+    def verify_administrator
+      unless current_user&.administrator?
+        redirect_to root_path, alert: "No tienes permisos para acceder a esta página."
+      end
+    end
 end
