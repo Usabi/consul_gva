b83ed01554792d90731a4d91dcbca8fd7715a05e
Alejandro Vera <avera@usabi.es>
2026-06-16 08:58:17 +0100

    chore: actualizar rama a issue/8435-ruby-3.3.8 y añadir Ruby 3.3.8 en USABI_ISSUE

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch, "issue/8435-update-2.3.1-custom"
+set :branch, "issue/8435-ruby-3.3.8"
 
 server deploysecret(:server), user: deploysecret(:user), roles: %w[web app db importer cron background]
 # server main_deploy_server, user: deploysecret(:user), roles: %w[web app db importer cron background]
