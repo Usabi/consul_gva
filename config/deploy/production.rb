743ee53fa7e6366a59c83fd25341f3c779d9ae4b
Alejandro Vera <avera@usabi.es>
2026-06-29 09:29:13 +0100

    chore: actualizar Ruby de 3.3.8 a 3.3.10

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch, "issue/8435-ruby-3.3.8"
+set :branch, "issue/8435-ruby-3.3.10"
 
 server deploysecret(:server), user: deploysecret(:user), roles: %w[web app db importer cron background]
 # server main_deploy_server, user: deploysecret(:user), roles: %w[web app db importer cron background]
