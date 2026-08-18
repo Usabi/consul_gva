8b0bdaf5e58492f1195be214e3b359774bc29764
Alejandro Vera <avera@usabi.es>
2026-08-18 12:39:08 +0100

    fix: arreglar errores de ESLint (5 ficheros preexistentes)
    El job ESLint fallaba en cualquier PR por deuda acumulada en 5
    ficheros:
    
    - html_editor.js: espaciado tras "if"/"else", punto y coma, línea en
      blanco antes del cierre del IIFE.
    - investment_report_alert.js: indentación desalineada.
    - select_local.js: comillas simples, "use strict" sin forma función,
      parámetro sin usar.
    - sortable.js y welcome_counter.js: usaban sintaxis ES6 (arrow
      functions, class) que el parser del proyecto no soporta
      (.eslintrc.json fija ecmaVersion: 5, todo el resto del código usa
      var/function). Se convierten a ES5 (function+prototype en vez de
      class, function expressions en vez de arrow functions) para quedar
      consistentes con el resto de app/assets/javascripts/custom. Cambio
      puramente sintáctico, mismo comportamiento.
    
    welcome_counter.js no está referenciado desde ninguna vista
    actualmente (sin .welcome-counter en app/views), así que no hay
    riesgo de regresión visible.

--- app/assets/javascripts/custom/select_local.js
+++ app/assets/javascripts/custom/select_local.js
@@ -1,5 +1,9 @@
-document.addEventListener('turbolinks:load', function(event) {
-  $("input[type=radio][name=select-local]").on("change", function () {
-    window.location.assign($(this).val());
+(function() {
+  "use strict";
+
+  document.addEventListener("turbolinks:load", function() {
+    $("input[type=radio][name=select-local]").on("change", function() {
+      window.location.assign($(this).val());
+    });
   });
-});
+}).call(this);
